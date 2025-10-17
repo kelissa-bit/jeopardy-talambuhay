2b6bd0e6d899ff9d25d9cdde4b2f2107b5187a53
Kelissa <kelissa_wallace@horacemann.org>
2025-10-17 09:04:14 -0400

    changed questions and answers, added 3 more future questions, changed category names

--- server/db.ts
+++ server/db.ts
@@ -1,31 +1,28 @@
 import type { PlayerData, Question } from '$lib/index';
 
 const playerData: PlayerData[] = [];
-const TIME_LEFT = 8; // seconds
+const TIME_LEFT = 10; // seconds
 const sortQuestions = (questions: { points: number; question: string; answer: string; imgSrc?: string; }[]) => questions.sort((a, b) => a.points - b.points).map(q => ({ ...q, answered: false, buzzers: [] as string[] }));
 const pastQuestions: Question[] = sortQuestions([
     {
         points: 100,
-        question: 'question 1',
-        answer: 'Illinois',
+        question: 'What is the 50th U.S. State?',
+        answer: 'Hawaii',
     },
     {
         points: 200,
-        question:
-            'question 2',
-        imgSrc: "https://cdn.britannica.com/34/4034-050-91EE1BCF/Flag-Myanmar.jpg",
-        answer: 'Myanmar',
-    },
-    {
-        points: 300,
-        question:
-            'What Ivy League school has the highest Native American enrollment (a whoppping 1%)?',
-        answer: 'Dartmouth',
+        question: 'What was last year\'s UD Spring Musical?',
+        answer: 'Chicago',
     },
     {
         points: 400,
-        question: 'Who wrote the Critique of Pure Reason?',
-        answer: 'Immanuel Kant',
+        question: 'What is the name of the boba shop next to Fieldston?',
+        answer: 'Pearl Dynastea',
+    },
+    {
+        points: 300,
+        question: 'What type of hacking is offered at this school?',
+        answer: 'Ethical Hacking',
     }
 ]);
 
@@ -33,56 +30,60 @@ const presentQuestions: Question[] =
     sortQuestions([
         {
             points: 200,
-            question:
-                'What kind of rock is this?',
-            imgSrc: 'https://madera.objects.liquidweb.services/photos/16842-half-dome-closeup-from-glacier-point-steve-montalto-hmi-Rectangle-600x400.jpg',
-            answer: 'Granite',
+            question: 'What is the study of the structure of the body?',
+            answer: 'Anatomy',
         },
         {
             points: 100,
-            question:
-                'What is this Cafe called?',
-            imgSrc: 'https://lh3.googleusercontent.com/p/AF1QipNsmB0ugJeJxYVrBKpRkNkyiEa6cKLamFZ4r0M=s1360-w1360-h1020',
-            answer: 'Chaotic Good',
+             question: 'What city is named \"The Big Apple?\"',
+            answer: 'New York City', 
         },
         {
             points: 300,
-            question: 'What programming language is the below code?',
-            imgSrc: '/programming_language.png',
-            answer: 'Javascript',
+            question: 'What month and day is Veterans Day?',
+            answer: 'November 11', 
         },
         {
             points: 400,
-            question:
-                'Who painted this?',
-            imgSrc:
-                "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjb1tCOwOdOeYcp5iflCvvW95qCqpmNUo-TMIt3ndxzsxzmgmH18iClIIQLPO48ojPg5Rts2AUm9rZBeVPcjnjrjGaLSzCwbipQotY4EhOk3tUoHJjJyZjTqfY5s9MZ5eSkGrrqmom4JXUdHEqE-Ts8E9i-SuFf9xEukJcFBs5NuOhe6ANdODMFYzyV_Q/s16000/Unfinished.jpg",
-            answer: 'Keith Haring',
+            question: 'What time does I period end?',
+            answer: '4:00',
         }
     ]);
 const futureQuestions: Question[] = sortQuestions([
     {
         points: 100,
-        question:
-            'This country is home to the Dolomites, which are a mountain range that has historical \'via ferratas\', iron cables and rungs, to aid traversing the peaks?',
-        imgSrc:
-            "https://laguidalpina.it/cdn/shop/products/ferrata-marmolada-cresta-ovest-Cristiano-Gregnanin-Guida-Alpina-Certificata-Dolomiti-5.jpg?v=1738870778",
+        question: 'What country is oddly shaped like a boot?', 
         answer: 'Italy',
+    },
+    {
+        points: 200,
+        question: 'What is this year\'s UD Spring Musical',
+        answer: 'Oklahoma',
+    },
+    {
+        points: 300,
+        question: 'Which play does the quote "to be or not to be, that is the question" come from',
+        answer: 'Hamlet',
+    },
+    {
+        points: 400,
+        question: 'What is the name of the book for Book Day 2026?',
+        answer: 'Beyond Measure',
     }
 ]);
 
 
 const categories = [
     {
-        title: 'My Past',
+        title: 'Kelissa\'s Past',
         questions: pastQuestions
     },
     {
-        title: 'My Present',
+        title: 'Kelissa\'s Present',
         questions: presentQuestions
     },
     {
-        title: 'My Future',
+        title: 'Kelissa\'s Future',
         questions: futureQuestions
     }
 ];
